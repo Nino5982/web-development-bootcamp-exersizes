b9d50049efad110d1006256c55d5ed146d179252
nino <ninosopoian@gmail.com>
2024-12-20 21:58:06 +0400

    add passing data code, and  adding some styles and static files to ejs file

--- 4.2+Passing+Data/index.js
+++ 4.2+Passing+Data/index.js
@@ -6,9 +6,15 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
-
-app.post("/submit", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+app.post("/submit", (req, res) => {
+  let fname = req.body["fName"];
+  let lName = req.body["lName"];
+  let numberOfLetters = fname.length + lName.length;
+  res.render("index.ejs", { numLetters: numberOfLetters });
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
